ffcf23958779f31e55b457815eb0b1f38a707c24
Gabriel Gomes <bielgpereira01@gmail.com>
2024-05-10 13:22:17 -0300

    Update cnc_api_discord.php

--- app/auth/discord/cnc_api_discord.php
+++ app/auth/discord/cnc_api_discord.php
@@ -1,6 +1,5 @@
 <?php 
 $urlTeste = "";
-// $discord_url = "https://discord.com/api/oauth2/authorize?client_id=1004426962342785114&redirect_uri=https%3A%2F%2Fwww.punkstudios.com.br%2Fsite%2Foauth_discord.php&response_type=code&scope=identify%20email%20connections%20guilds.join%20guilds";
 echo $urlTeste;
 exit();
 ?>
